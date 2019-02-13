cd55801be34d119464f7298683f0f3558b12c12d
mra <tad)goW>
2019-02-13 16:15:17 +0100

    finished demo job, api and widget

--- mypro/job.py
+++ mypro/job.py
@@ -3,6 +3,7 @@ import math
 from core4.queue.helper.job.base import CoreLoadJob
 from core4.queue.helper.functool import enqueue
 
+
 def check_prime(n):
     if n % 2 == 0:
         return False
@@ -32,6 +33,36 @@ class PrimeJob(CoreLoadJob):
                 self.logger.info("found [%d]", i)
 
 
+class PrimeJobControl(CoreLoadJob):
+    author = "mra"
+    defer_time = 5
+    max_parallel = 3
+
+    def execute(self, start=None, end=None, size=None, mid=None, count=None,
+                **kwargs):
+        if size:
+            if self.trial > 1:
+                running = self.config.sys.queue.count_documents(
+                    {"args.mid": str(self._id)})
+                if running > 0:
+                    self.defer("check: waiting for %d jobs to complete", running)
+                self.logger.info("check: seen all jobs complete")
+                return
+            for i in range(start, end, size):
+                e = i + size
+                if e > end:
+                    e = end
+                enqueue(PrimeJobControl, start=i, end=e, mid=str(self._id),
+                        **kwargs)
+            self.defer("waiting")
+        else:
+            for i in range(start, end):
+                self.progress(i/end)
+                if check_prime(i):
+                    print("found [%d]" %(i))
+                    self.logger.info("found [%d]", i)
+
+
 if __name__ == '__main__':
     from core4.queue.helper.functool import execute
-    execute(PrimeJob, start=1, end=1000000)
+    execute(PrimeJobControl, start=1, end=3000000, size=500000)
--- mypro/server.py
+++ mypro/server.py
@@ -1,24 +1,39 @@
 from core4.api.v1.request.main import CoreRequestHandler
 from core4.api.v1.application import CoreApiContainer
+from core4.queue.helper.functool import enqueue
 
 
-class TestHandler(CoreRequestHandler):
+class PrimeHandler(CoreRequestHandler):
 
     author = "mra"
-    title = "test handler"
+    title = "prime job handler"
 
     def get(self):
-        self.reply("hello world")
+        return self.post()
 
+    def post(self):
+        start = self.get_argument("start", as_type=int, default=None)
+        end = self.get_argument("end", as_type=int, default=None)
+        size = self.get_argument("size", as_type=int, default=None)
+        if None in (start, end, size):
+            return self.render("templates/prime.html", job_id=None)
+        kwargs = {
+            "start": start,
+            "end": end,
+            "size": size
+        }
+        job = enqueue("mypro.job.PrimeJobControl", **kwargs)
+        if self.wants_html():
+            return self.render("templates/prime.html", job_id=str(job._id))
+        return self.redirect("http://devops:5002"
+                             "/coco/v1/jobs/poll/" + str(job._id))
 
-
-class TestServer(CoreApiContainer):
-    root = "/test-server"
+class PrimeServer(CoreApiContainer):
     rules = [
-        (r'/test', TestHandler)
+        (r'/prime', PrimeHandler)
     ]
 
 
 if __name__ == '__main__':
     from core4.api.v1.tool.functool import serve
-    serve(TestServer)
+    serve(PrimeServer)
